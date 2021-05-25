b922f18c0366e4707b50ebc3197f2c21a854d200
eigertec07 <49597343+eigertec07@users.noreply.github.com>
2021-05-25 12:19:40 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,34 +1,43 @@
+let play = false
+input.onButtonPressed(Button.A, function () {
+    play = true
+})
+input.onButtonPressed(Button.B, function () {
+    play = false
+})
 basic.forever(function () {
-    basic.showLeds(`
-        . . . . .
-        # . . . .
-        . . . . .
-        . . . . .
-        . . . . .
-        `)
-    music.playTone(831, music.beat(BeatFraction.Eighth))
-    basic.showLeds(`
-        . # . . .
-        . . . . .
-        . . . . .
-        . . . . .
-        . . . . .
-        `)
-    music.playTone(880, music.beat(BeatFraction.Eighth))
-    basic.showLeds(`
-        . . . . .
-        . . # . .
-        . . . . .
-        . . . . .
-        . . . . .
-        `)
-    music.playTone(831, music.beat(BeatFraction.Eighth))
-    basic.showLeds(`
-        . . . . .
-        . . . . .
-        . . . # .
-        . . . . .
-        . . . . .
-        `)
-    music.playTone(659, music.beat(BeatFraction.Eighth))
+    if (play) {
+        music.playTone(831, music.beat(BeatFraction.Eighth))
+        basic.showLeds(`
+            . . . . .
+            # . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            `)
+        music.playTone(880, music.beat(BeatFraction.Eighth))
+        basic.showLeds(`
+            . # . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            `)
+        music.playTone(831, music.beat(BeatFraction.Eighth))
+        basic.showLeds(`
+            . . . . .
+            . . # . .
+            . . . . .
+            . . . . .
+            . . . . .
+            `)
+        music.playTone(659, music.beat(BeatFraction.Eighth))
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . # .
+            . . . . .
+            . . . . .
+            `)
+    }
 })
